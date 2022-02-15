660bf46d984210b8d55e341e330c89db09f8221d
Josep Suarez <djose.anton@gmail.com>
2022-02-15 10:46:55 +0100

    Add all functionality to ToDo list

--- vue-test/src/components/BodyComponent.vue
+++ vue-test/src/components/BodyComponent.vue
@@ -1,8 +1,14 @@
 <template>
     <div>
-        <h1>Componente</h1>
-        <h2>{{subtitle}}</h2>
-        <button v-on:click="consoleLog">Click</button>
+        <p>{{text}}</p>
+        <ul>
+            <li v-for="toDo in toDoList" :key="toDo.sentence" v-bind:style="[toDo.done === true ? {color: 'red'} : {color: 'black'}]">
+                {{ toDo.sentence }}
+                <button v-on:click="doneFunction(toDo)">Done</button>
+                <button v-on:click="deleteFunction(toDo)">Borrar</button>
+            </li>
+        </ul>
+         <button v-on:click="addTodo">Añadir</button>
     </div>
 </template>
 
@@ -11,17 +17,43 @@ export default {
   name: 'BodyComponent',
   data(){
       return {
-          subtitle: "Subtitulo de mi componente"
+          text: "Esta es tu To-Do List, cuándo hagas la tarea pulsa el botón 'Done', si quieres borrarla pulsa el botón 'Borrar'y si deseas otra tarea pulsa el botón 'Añadir'",
+          toDoList: [{
+            done: false,
+            sentence: "Comprar leche"
+          },
+          {
+            done: false,
+            sentence: "Ir al recoger el coche al taller"
+          },
+          {
+            done: false,
+            sentence: "Hacer los deberes"
+          }
+          ]
       }
   },
   methods: {
-      consoleLog: function(){
-          console.log("Has clickado el botón")
+      doneFunction: function(doneTodo){
+          this.toDoList.map(toDo => {
+              if(toDo.sentence === doneTodo.sentence){
+                  toDo.done = !toDo.done
+              }
+          })
+      },
+      deleteFunction: function(deleteTodo){
+          this.toDoList = this.toDoList.filter(toDo => toDo.sentence !== deleteTodo.sentence)
+      },
+      addTodo: function(){
+          let toDotoAdd = prompt("Que toDo quieres añadir?")
+          this.toDoList.push({done: false, sentence: toDotoAdd})
       }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+ul {
+    list-style: none;
+}
 </style>

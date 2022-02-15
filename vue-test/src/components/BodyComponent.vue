9d7f182e50457b09f0f5d43eafb3814a72b73149
Josep Suarez <djose.anton@gmail.com>
2022-02-15 11:22:55 +0100

    End project with vue

--- vue-test/src/components/BodyComponent.vue
+++ vue-test/src/components/BodyComponent.vue
@@ -2,13 +2,13 @@
     <div>
         <p>{{text}}</p>
         <ul>
-            <li v-for="toDo in toDoList" :key="toDo.sentence" v-bind:class="[toDo.done === true ? activeClass : 'done', errorClass]">
+            <li v-for="toDo in toDoList" :key="toDo.sentence" v-bind:class="[toDo.done === false ? activeClass : 'done', errorClass]">
                 {{ toDo.sentence }}
-                <button v-on:click="doneFunction(toDo)">Done</button>
-                <button v-on:click="deleteFunction(toDo)">Borrar</button>
+                <button v-on:click="doneFunction(toDo)" v-bind:class="[toDo.done === false ? 'buttonNotDone' : 'buttonDone', errorClass]">Done</button>
+                <button v-on:click="deleteFunction(toDo)" class="buttonNotDone">Borrar</button>
             </li>
         </ul>
-         <button v-on:click="addTodo">Añadir</button>
+         <button v-on:click="addTodo" class="buttonNotDone">Añadir</button>
     </div>
 </template>
 
@@ -55,6 +55,36 @@ export default {
 <style scoped>
 ul {
     list-style: none;
+}
+.buttonNotDone {
+    border-radius: 5px;
+    background:rgb(139, 199, 255);
+    -moz-box-shadow: 0px 5px 0px 0px rgb(0, 105, 202);
+    -webkit-box-shadow: 0px 5px 0px 0px rgb(0, 105, 202);
+    box-shadow: 0px 5px 0px 0px rgb(0, 105, 202);
+    display:inline-block;
+    margin: 10px 0px 10px 10px;
+    padding: 5px;
+    height: 30px;
+    text-decoration:none;
+    transition: all 0.10s ease-in-out;
+    -webkit-transition: all 0.10s ease-in-out;
+    position:relative;
+}
+.buttonDone {
+    border-radius: 5px;
+    background:rgb(77, 241, 132);
+    -moz-box-shadow: 0px 5px 0px 0px rgb(0, 105, 202);
+    -webkit-box-shadow: 0px 5px 0px 0px rgb(0, 105, 202);
+    box-shadow: 0px 5px 0px 0px rgb(0, 105, 202);
+    display:inline-block;
+    margin: 10px 0px 10px 10px;
+    padding: 5px;
+    height: 30px;
+    text-decoration:none;
+    transition: all 0.10s ease-in-out;
+    -webkit-transition: all 0.10s ease-in-out;
+    position:relative;
 }
 .done {
     text-decoration: line-through;
